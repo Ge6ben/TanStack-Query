209dc756e02cd91802c57d38499c9d39e3245038
Ge6ben <dl22gesh40@gmail.com>
2024-10-19 15:42:23 +0300

    ref:apply eslint

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,12 @@
-
 <script lang="ts" setup>
- import { useQuery } from '@tanstack/vue-query';
+  import { useQuery } from '@tanstack/vue-query'
 
   const fetchData = async () => {
-   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-   return response.json()
- }
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    return response.json()
+  }
 
- const { data } = useQuery(['todo'], fetchData)
-
-
-
-// const useUpload = ({ queryKey }: { queryKey: Ref<{ sortModel: number }> }) => {
-//
-//   return useQuery(
-//     // Query key for Vue Query - make sure it's reactive
-//     ['add-upc-scanner', queryKey.value],
-//     async () => {
-//       const { data, error } = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
-//         method: 'POST',
-//         body: JSON.stringify(queryKey.value)
-//       });
-//
-//       if (error) {
-//         throw new Error(error.message);
-//       }
-//
-//       return data;
-//     },
-//     {
-//       enabled: true
-//     }
-//   );
-// };
-
-// const getQueryKey = computed(() => ({
-//   sortModel: sortModel.value
-// }));
-// const { data: myData } = useUpload({ queryKey: getQueryKey });
+  const { data } = useQuery(['todo'], fetchData)
 
  </script>
 
