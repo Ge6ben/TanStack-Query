47774436ec065e0ca5216045c096318601510f0b
Ge6ben <dl22gesh40@gmail.com>
2024-10-19 15:44:24 +0300

    feat: add @tanstack/vue-query-devtools

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
   import { useQuery } from '@tanstack/vue-query'
+  import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 
   const fetchData = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
@@ -13,5 +14,7 @@
 <template>
   <div class="text-right">
     {{ data }}
+    <VueQueryDevtools />
+
   </div>
 </template>
