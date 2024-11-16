d39a8d34eef195d0fa71a9aad0f7b68158aa22ed
Ge6ben <dl22gesh40@gmail.com>
2024-11-16 11:50:30 +0300

    fix the calling functions

--- src/fetchDataCompose.ts
+++ src/fetchDataCompose.ts
@@ -1,23 +1,19 @@
-// const { data, error, isLoading, isError } = useGetDoctors({ queryKey: imageUrl });
 import { useQuery } from '@tanstack/vue-query'
 import { Ref } from 'vue'
 
 export const useGetData = ({ queryKey }: { queryKey: Ref<string> }) => {
-  return useQuery(
-    // The queryKey is used to identify and cache the query
-    ['get-data', queryKey.value],
-    async () => {
-      // Query data from Supabase
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const fetchData = async () => {
+    // Query data from Supabase
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-      return response.json()
-    }
-    // {
-    //   enabled: !!queryKey.value
-    //   // The query is disabled if queryKey is not provided or empty
-    //   // enabled: !!queryKey.value, // Make sure the queryKey is valid before running the query
-    //   // staleTime: 1000 * 60 * 5, // Cache data for 5 minutes
-    //   // retry: 2 // Retry failed queries 2 times
-    // }
-  )
+    return data.json()
+  }
+
+  return useQuery({
+    queryKey: ['get-data', queryKey.value], // Specify queryKey in an object
+    queryFn: fetchData, // Use  it as query function
+    // enabled: !!queryKey.value, // Other options as needed
+    staleTime: 1000 * 60 * 5, // Cache data for 5 minutes
+    retry: 2, // Retry failed queries 2 times
+  })
 }
